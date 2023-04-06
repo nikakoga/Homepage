dcfd0aaaa05acc5f4f2017835a1d7953cfb3d7e6
nikakoga <weronikakoga@gmail.com>
2023-04-06 21:32:57 +0200

    strona domowa, notes oraz usefull links funkcjonalne, ale wciaz do upiekszenia

---
+++ main.js
@@ -0,0 +1,7 @@
+alert('Hejka okruszku !😊') //alert który wyskoczy na stronie
+console.log('No heeej widze, że ktoś tu szpera 😎')
+const my_name = 'Nika';
+let age = 22;
+console.log(`My name is ${my_name} and I'm ${age} years old`);
+let bye = "No to hejkaaa 🚀";
+console.log(bye);
